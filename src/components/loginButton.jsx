9e14e5efe7328c07c93bef265413eb7c6c869e50
chejito <smndz81@gmail.com>
2022-01-07 02:54:11 +0100

    Botón redondeado

--- src/components/loginButton.jsx
+++ src/components/loginButton.jsx
@@ -8,7 +8,8 @@ const LoginButton = (props) => {
       backgroundColor: 'var(--primary-color)',
       color: 'var(--secondary-bg-color)',
       fontSize: '15px',
-      fontWeight: 700
+      fontWeight: 700,
+      borderRadius: '8px'
     }}>
       {props.value}
     </button>
